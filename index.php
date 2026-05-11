7218f0d35a35a16caf91dc4c373cb5e7047cff0e
Afrid2004 <mdfaisalafrid@gmail.com>
2026-05-11 11:04:38 +0600

    number variable declare & arithmetic operations

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php 
+    echo 'Hello world <br>';
+
+    // string variable declare
+    $firstName = 'Faisal';
+    $lastName = 'Yousuf';
+
+    //print variables like string litarel
+    echo "$firstName $lastName";
+    echo "</br>";
+
+    //number variable declare & arithmetic operations
+    $num1 = 10;
+    $num2 = 5;
+    $sum = $num1 + $num2;
+    $sub = $num1 - $num2;
+    $mul = $num1 * $num2;
+    $div = $num1 / $num2;
+    $mod = $num1 % $num2;
+
+    //2 numbers are
+    echo "Num1 = $num1, Num2 = $num2 </br>";
+
+    //sum of 2 number
+    echo "Sum is $sum </br>";
+
+    //sub of 2 number
+    echo "Sub is $sub </br>";
+
+    //multiplication of 2 number
+    echo "Multiplication is $mul </br>";
+
+    //divition of 2 number
+    echo "Divition is $div </br>";
+
+    //Modulas of 2 number
+    echo "Modulas is $mod </br>";
+    
+?>
+
