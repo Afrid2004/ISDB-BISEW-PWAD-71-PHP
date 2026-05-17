a9ef4529ecdb77058e8be6851cb62ae081923e46
Afrid2004 <mdfaisalafrid@gmail.com>
2026-05-17 12:07:31 +0600

    Added Sprintf, Printf, string Operations

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <?php 
+
+    echo "===================PRINT OPERATION===================" . "<br>";
     echo 'Hello world <br>';
+    echo 'This is ', 'Multiple ', 'string ', '</br>';
+
+    //printf with placeholder (direct print)
+    $myName = "Afrid";
+    printf("Printf String: Hello, %s</br>", $myName);
+    $myGpa = 4.89;
+    printf("Printf Float: My GPA: %.2f</br>", $myGpa);
+
+    //sprintf with placeholder (doesn't print directly, only return)
+    $mySprintName= "Rayhan";
+    $rayhanGPA = 4.25;
+    echo sprintf("Sprintf String & Float: Hello, %s. Tumi GPA %.2f peyecho out of 5.00", $mySprintName, $rayhanGPA) . '</br>';
 
     // string variable declare
     $firstName = 'Faisal';
     $lastName = 'Yousuf';
 
     //print variables like string litarel
-    echo "$firstName $lastName";
+    echo "Echo: $firstName $lastName";
     echo "</br>";
 
+    echo "===================ARITHMETIC OPERATION===================" . "<br>";
     //number variable declare & arithmetic operations
     $num1 = 10;
     $num2 = 5;
@@ -36,27 +51,43 @@
     //Modulas of 2 number
     echo "Modulas is $mod </br>";
 
-
+    echo "===================VARIABLE & DATA TYPES===================" . "<br>";
     //constant variables
     const PI = 3.1416;
     //print constant variables with string
     echo "Value of PI is " . PI . "</br>";
 
+    //for type show use var dump
+    $float = 4.50;
+    echo "Data Types: ";
+    var_dump($float) ;
+    echo "</br>";
+
+    echo "===================ARRAY & ASSOSIATIVE ARRAY===================" . "<br>";
     //array
     $students = ["Faisal", "Rayhan", "Afrid"];
     //print array with print_r
     echo "Normal Array: ";
     print_r($students);
     echo "</br>";
+    echo "As a String in pre tag: <pre>".print_r($students,true)."</pre>";
 
     //assosiative array key pare value like JS Object
-    $Faisal = ["name" => "Faisal Yousuf Afrid", "age" => 22, "address" => "Gulshan, Dhaka"];
+    $Assosiative = ["name" => "Faisal Yousuf Afrid", "age" => 22, "address" => "Gulshan, Dhaka"];
     //print full assosiative array with print_r
     echo "Assosiative Array: ";
-    print_r($Faisal);
+    print_r($Assosiative);
     echo "</br>";
-    //print assosiative array specific data with print_r
-    echo "Assosiative array specific data: {$Faisal["address"]}";
+    //print assosiative array specific data with echo
+    echo "Assosiative array specific data: {$Assosiative["address"]}" . "<br>";
 
+    echo "===================STRING OPERATIONS===================" . "<br>";
+    $string = "This is a String ";
+    echo "String: ". $string . "<br>";
+    echo "String Length: ". strlen($string) . "<br>";
+    echo "String Uppercase: ". strtoupper($string) . "<br>";
+    echo "String Lowercase: ". strtolower($string) . "<br>";
+    echo "String Trim: ". trim($string) . "<br>";
+    echo "String Replace: ". str_replace("String", "Replaced String", $string) . "<br>";
 ?>
 
